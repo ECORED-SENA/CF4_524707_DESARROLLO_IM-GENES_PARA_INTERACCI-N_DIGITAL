a8c30d4decca0d9fe5859e61d721abecde00a454
halvarezast <halvarezast@gmail.com>
2026-03-11 03:31:44 -0500

    Update331

--- src/config/global.js
+++ src/config/global.js
@@ -94,11 +94,6 @@ export default {
         titulo: 'Glosario',
       },
       {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
-      {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
         nombreRuta: 'referencias',
